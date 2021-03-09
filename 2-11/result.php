961512ff4b9fbdf2cdef26318198a261a1f24512
yujiasano <forever2011.0903@gmail.com>
2021-03-09 21:30:47 +0900

    フォームに入力した内容を取得する 課題

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $email; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
