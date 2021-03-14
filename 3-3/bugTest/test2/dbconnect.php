1c8917a8e62dd6510893b54f4d6a46ba7cf3f834
yujiasano <forever2011.0903@gmail.com>
2021-03-14 19:16:51 +0900

    バグ修正　課題

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "YIGroupBlog";
+// $db['dbname'] = "loginManagement";
